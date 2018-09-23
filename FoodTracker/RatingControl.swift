c645b897ec3ce85750fde3e05fc833c304a1d879
psx95 <pranav.ps95@hotmail.com>
2018-09-23 23:04:59 +0530

    feat(UI): Adds Custom View and attaches actions to it

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Pranav Sharma on 23/09/18.
+//  Copyright © 2018 Pranav Sharma. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialisation
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍🏻")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        // Add Constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        //Adds button to the stack
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        addArrangedSubview(button)
+    }
+}
